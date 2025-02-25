2863fe90292d6be8b09ea6f61cd018e01ebd6a6e
DilanGerber <130708466+DilanGerber@users.noreply.github.com>
2025-02-25 04:17:12 -0300

    estructura inicial de todo

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Experience from "@/components/home/experience/Experience";
 import Landing from "@/components/home/Landing";
+import Projects from "@/components/home/projects/Projects";
+import Skills from "@/components/home/Skills";
 import ThemeSwitcher from "@/components/theme/ThemeSwitcher";
 
 export default function Home() {
@@ -7,7 +9,9 @@ export default function Home() {
     <div className=" ">
       <ThemeSwitcher/>
       <Landing />
+      <Projects />
       <Experience />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/home/projects/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <section className=' relative px-4 mt-10 max-w-[1000px] mx-auto'>
+        <h2 className="text-3xl lg:text-4xl font-bold font-inter text-green-700 text-center">Proyectos</h2>
+    </section>
+  )
+}
+
+export default Projects
