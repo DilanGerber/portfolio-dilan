0e33643f94cf5dfb06b46c975c8b49b0570ca016
DilanGerber <130708466+DilanGerber@users.noreply.github.com>
2025-02-25 02:01:27 -0300

    cambio de fonts y termine Landing menos la foto

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,24 +1,24 @@
 import Image from 'next/image'
 import React from 'react'
-import IconDownload from '../icons/IconDownload'
-import IconGitHub from '../icons/IconGitHub'
-import IconLinkedin from '../icons/IconLinkedin'
-import IconLocation from '../icons/IconLocation'
-import IconMail from '../icons/IconMail'
+import IconGitHub from '../../icons/IconGitHub'
+import IconLinkedin from '../../icons/IconLinkedin'
+import IconLocation from '../../icons/IconLocation'
+import IconMail from '../../icons/IconMail'
+import IconCV from '@/icons/IconCV'
 
 const Landing = () => {
   return (
     <section className=' relative px-4 grid max-w-[1000px] mx-auto md:grid-cols-2'>
-        <div className='flex flex-col justify-center'>
-            <h1 className="text-4xl font-bold">¡Hola! Soy <span className=' text-green-700' aria-label="Dilan Gerber">Dilan Gerber</span>, Desarrollador Web</h1>
+        <div className='flex flex-col justify-center '>
+            <h1 className="text-3xl lg:text-4xl font-bold font-inter">¡Hola! Soy <span className=' text-green-700' aria-label="Dilan Gerber">Dilan Gerber</span>, Desarrollador Web</h1>
             <p className="text-lg font-light max-w-xl mx-auto mt-4">
-                Full Stack Developer especializado en desarrollo back-end, con experiencia en la creación de soluciones rápidas y escalables, creando sitios web optimizados y responsivos.
+                Full Stack Developer especializado en desarrollo <span className='whitespace-nowrap font-semibold'>back-end</span>, con experiencia en la creación de soluciones rápidas y escalables, creando sitios web optimizados y responsivos.
             </p>
             <div className='flex flex-row items-center gap-4 mt-4'>
                 <IconLocation className="size-6" />
                 <p>La Paz - Bolivia</p>
             </div>
-            <div className="flex items-center gap-6 mt-4">
+            <div className="flex items-center gap-3 sm:gap-6 mt-4">
                 <a 
                     href="https://github.com/dilangerber" 
                     target="_blank" 
@@ -41,19 +41,20 @@ const Landing = () => {
                     href="https://linkedin.com/in/dilangerber" 
                     target="_blank" 
                     rel="noopener noreferrer" 
-                    className="py-2 px-4  border-2 border-green-700 rounded-full text-green-700 hover:bg-green-800 hover:text-white transition-colors flex flex-row gap-3"
+                    className="lg:py-2 lg:px-4 p-2 border-2 border-green-700 rounded-full text-green-700 hover:bg-green-800 hover:text-white transition-colors flex flex-row gap-3 whitespace-nowrap"
                     aria-label="LinkedIn de Dilan Gerber"
                 >
-                    <IconDownload className="size-6" /> Descargar CV
+                    <IconCV className="size-6" />
+                    <span className="hidden lg:block">Descargar CV</span>
                 </a>
                 <a 
                     href="https://linkedin.com/in/dilangerber" 
                     target="_blank" 
                     rel="noopener noreferrer" 
-                    className="py-2 px-4  border-2 border-green-700 rounded-full text-green-700 hover:bg-green-800 hover:text-white transition-colors flex flex-row gap-3"
+                    className="py-2 px-4  border-2 border-green-700 rounded-full text-green-700 hover:bg-green-800 hover:text-white transition-colors flex flex-row gap-3 whitespace-nowrap"
                     aria-label="LinkedIn de Dilan Gerber"
                 >
-                    <IconMail className="size-6" /> Copiar Mail
+                    <IconMail className="size-6" /> Copiar Email
                 </a>
             </div>
         </div>
---
+++ src/icons/IconCV.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+const IconCV = (props) => (
+  <svg
+    stroke="currentColor"
+    fill="none"
+    strokeWidth={2}
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+    <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+    <path d="M10 9H8" />
+    <path d="M16 13H8" />
+    <path d="M16 17H8" />
+  </svg>
+);
+export default IconCV
