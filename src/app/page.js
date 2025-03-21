ac442cba795a75a7106d9ff736c09d8f29ac85af
DilanGerber <130708466+DilanGerber@users.noreply.github.com>
2025-03-20 21:03:03 -0300

    sin calendar

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Landing from "@/components/home/Landing";
 import Projects from "@/components/home/projects/Projects";
 import Skills from "@/components/home/Skills";
 import StartButton from "@/components/layout/StartButton";
-import GitHubActivity from "@/components/ui/GitHubActivity";
+// import GitHubActivity from "@/components/ui/GitHubActivity";
 
 export default function Home() {
   return (
@@ -13,9 +13,13 @@ export default function Home() {
       <Experience />
       <Projects />
       <Skills />
-      <GitHubActivity />
+      {/* <GitHubActivity /> */}
       <AboutMe />
       <StartButton />
     </main>
   );
 }
+
+// performance.getEntriesByType("resource")
+//   .filter(r => r.initiatorType === "script")
+//   .map(r => ({ name: r.name, duration: r.duration }))
