9c6567801fa1fcf9f5af7418323121a6a541dc00
DilanGerber <130708466+DilanGerber@users.noreply.github.com>
2025-03-21 02:59:34 -0300

    SkillsSlider optimizado

--- src/components/ui/SkillsSlider.jsx
+++ src/components/ui/SkillsSlider.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+
+import { useState, useEffect } from "react";
 const filaOneTechnologies = ["Git", "Python", "TypeScript", "Cloudinary", "JWT", "MySQL", "Framer Motion", "Nodemailer", "Auth.js"]
 const filaTwoTechnologies = ["Redux", "Prisma", "PostgreSQL", "Firebase", "Docker", "ShadCN", "Thunder Client", "Figma", "React"]
 import { technologyIcons } from '@/lib/technologyIcons';
+import Image from 'next/image';
 
 const SkillsSlider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true); // 👈 Renderiza después de que la página se haya montado
+  }, []);
+
+  if (!isMounted) return null; // 👈 No renderiza hasta que el componente esté montado
+
   return (
     <div className="flex flex-col space-y-16 overflow-hidden mask-gradient py-16 bg-[radial-gradient(ellipse_at_center,_#111817_60%,_#111218)]">
     {/* Slider hacia la izquierda */}
@@ -11,9 +22,13 @@ const SkillsSlider = () => {
       <div className="flex space-x-16 animate-loop-scroll group-hover:paused ">
         {filaOneTechnologies.map((tech) => (
           <div key={tech} className="relative flex items-center justify-center">
-            <img
+            <Image
               src={technologyIcons[tech]}
               alt={tech}
+              width={80}
+              height={80}
+              loading="lazy"
+              priority={false}
               className="w-20 h-20 object-contain max-w-none flex-shrink-0 relative hover:z-10"
             />
             <div className="absolute inset-0 flex items-center justify-center 
