2dba3c06ac104d702c1e9d14ac7cb65a7178a75a
DilanGerber <130708466+DilanGerber@users.noreply.github.com>
2025-03-25 23:50:42 -0300

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning >
+      <head> 
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" /> 
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased ${inter.variable}`}
       >
